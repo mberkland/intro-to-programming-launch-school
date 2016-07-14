48f38cbc44ca72b4348fcd42df89442ba2d57d3a
Miriam Berkland <miruke0506@hotmail.com>
2016-07-13 22:23:24 -0400

    Fixing a typo

--- ITP-3-Variables/variables5_2.rb
+++ ITP-3-Variables/variables5_2.rb
@@ -1,4 +1,4 @@
-# This code gives an error because puts x(line 7) is trying to print out a variable that was not defined in the outer scope
+# This code gives an error because puts x(line 8) is trying to print out a variable that was not defined in the outer scope
 # x is defined inside of 3.times do and therefore can only be accessed from code inside of this inner scope 
 y = 0
 3.times do 
